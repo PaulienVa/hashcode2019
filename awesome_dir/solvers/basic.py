a6587367fde57d886f1f656678acf3c1de7e0142
Joosten, RKJ (Rosanne) <rosanne.joosten@rabobank.nl>
2019-02-28 19:50:38 +0100

    some perhaps useful functions

--- awesome_dir/solvers/basic.py
+++ awesome_dir/solvers/basic.py
@@ -2,6 +2,7 @@ import logging
 
 logger = logging.getLogger('solver/basic.py')
 
+# [{'nr': 0, 'nTags': '3', 'direction': 'H', 'tags': ['cat', 'beach', 'sun']}]
 
 def solve(photos):
     logger.info('Not actually solving anything')
@@ -14,8 +15,24 @@ def solve(photos):
 
 
     slides = []
-    vertical_slides = []
 
     logger.debug('Found some slides...')
 
     return slides
+
+
+def compareThem(photoA, photoB):
+    sameTags = 0
+    for photoAsTag in photoA["tags"]:
+        for photoBsTag in photoB["tags"]:
+            if photoAsTag == photoBsTag:
+                sameTags = sameTags + 1
+    return sameTags
+
+
+def returnSmallestSum(sums):
+    smallestSum = sums[0]
+    for sum in sums:
+        if smallestSum > sum:
+            smallestSum = sum
+    return smallestSum
