ed489090d202dbc5ace6c2c2e3a3d6cebb11a899
Paulien van Alst <paulien.vanalst@devolksbank.nl>
2019-02-28 19:28:19 +0100

    temp push

--- awesome_dir/solvers/basic.py
+++ awesome_dir/solvers/basic.py
@@ -6,16 +6,15 @@ logger = logging.getLogger('solver/basic.py')
 def solve(photos):
     logger.info('Not actually solving anything')
 
+    hor = filter(lambda x: x["direction"] == "H", photos)
+    vert = filter(lambda x: x["direction"] == "V", photos)
+
+    sorted_hor = sorted(hor, key=lambda x: x["nTags"])
+    sorted_vert = sorted(vert, key=lambda x: x["nTags"])
+
+
     slides = []
     vertical_slides = []
-
-    for photo in photos:
-        if photo["direction"] == 'H':
-            slides.append([photo["nr"]])
-        elif photo["direction"] == 'V':
-            vertical_slides.append(photo["nr"])
-    slides.append(vertical_slides)
-
 
     logger.debug('Found some slides...')
 
