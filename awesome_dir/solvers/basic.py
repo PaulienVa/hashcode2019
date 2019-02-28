63e75207b9752578602e6cc73ff299410863635e
Paulien van Alst <paulien.vanalst@devolksbank.nl>
2019-02-28 19:19:14 +0100

    basic very basic solver

--- awesome_dir/solvers/basic.py
+++ awesome_dir/solvers/basic.py
@@ -3,7 +3,20 @@ import logging
 logger = logging.getLogger('solver/basic.py')
 
 
-def solve(problem):
+def solve(photos):
     logger.info('Not actually solving anything')
-    logger.debug('Or debugging')
-    return problem
+
+    slides = []
+    vertical_slides = []
+
+    for photo in photos:
+        if photo["direction"] == 'H':
+            slides.append([photo["nr"]])
+        elif photo["direction"] == 'V':
+            vertical_slides.append(photo["nr"])
+    slides.append(vertical_slides)
+
+
+    logger.debug('Found some slides...')
+
+    return slides
